e27115f05efc9629e3eb23891879451d82c7109e
Nag Varun Chunduru <nchunduru@hearsaycorp.com>
2017-04-15 14:23:42 -0700

    Remove egg

--- CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-0.1.0 (2014-07-07)
-------------------
-    - Initial version.
-    - Add entities and client.
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 setup(
     name='json2csv',
-    version='0.0.2',
+    version='0.0.4',
     description='Converts nested json object to csv and csv back to json',
     author='Nag Varun Chunduru',
     author_email='cnv1989@gmail.com',
     long_description=(
-        open('README.md').read() + '\n\n' +
-        open('CHANGELOG.md').read()),
+        open('README.rst').read() + '\n\n' +
+        open('CHANGELOG.rst').read()),
     url='https://github.com/cnv1989/json2csv',  # use the URL to the github repo
     # I'll explain this in a second
-    download_url='https://github.com/cnv1989/json2csv/archive/v0.0.2.tar.gz',
+    download_url='https://github.com/cnv1989/json2csv/archive/v0.0.4.tar.gz',
     keywords=['json', 'csv', 'converter'],  # arbitrary keywords
     classifiers=[],
     package_dir={'': 'src'},
